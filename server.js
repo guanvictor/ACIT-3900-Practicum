a770a1901a4f09d7a80b14b293907b26df52e272
Kevin Yoo <k.ysyoobcit@gmail.com>
2019-10-03 21:11:36 -0700

    CSS addition and Endpoint creation

--- server.js
+++ server.js
@@ -15,8 +15,48 @@ hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (request, response) => {
+    response.render("home.hbs", {
+        title: "Home",
+        heading: "Home"
+    });
+});
+
+// Home Page
+app.get('/home', (request, response) => {
+    response.render("home.hbs", {
+        title:"Home",
+        heading: "Home"
+    });
+});
+
+// About Page
+app.get('/about', (request, response) => {
+    response.render("about.hbs", {
+        title:"About",
+        heading: "About"
+    });
+});
+
+// Registration Page
+app.get('/registration', (request, response) => {
     response.render("registration.hbs", {
-        title: "Registration",
+        title:"Registration",
         heading: "Registration"
+    });
+});
+
+// Agenda Page
+app.get('/agenda', (request, response) => {
+    response.render("agenda.hbs", {
+        title:"Agenda",
+        heading: "Agenda"
+    });
+});
+
+// Contact Page
+app.get('/contact', (request, response) => {
+    response.render("contact.hbs", {
+        title:"Contact",
+        heading: "Contact"
     });
 });

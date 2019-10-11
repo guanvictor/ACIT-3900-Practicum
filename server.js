dec899da992c1261a809a041e5ca3d49e43424a6
Cathy Chan <chan.cathy@hotmail.ca>
2019-10-10 18:53:28 -0700

    Passport and login portal
    Co-Authored-By: orangeappless <wilf.ta0072@gmail.com>

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 // Files
 const register = require("./registration.js");
 const db = require("./database.js");
+const passport = require("./passport.js");
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(register);
+app.use(passport);
 
 app.get("/", (request, response) => {
     response.render("home.hbs", {
@@ -34,11 +36,28 @@ app.get("/", (request, response) => {
     });
 });
 
-// Home Page
-app.get('/home', (request, response) => {
-    response.render("home.hbs", {
-        title:"Home",
-        heading: "Home"
+// Login Page
+app.get("/login", (request, response) => {
+    let sessionID = request.sessionID;
+    let sessionData_string = request.sessionStore.sessions[sessionID];
+    let sessionData = JSON.parse(sessionData_string);
+    let failureFlag = false;
+    let failureMessage = "";
+
+    if (sessionData.flash != undefined) {
+        failureFlag = true;
+        failureMessage = sessionData.flash.error[0];
+
+        delete sessionData["flash"];
+        let deletedError = JSON.stringify(sessionData);
+        request.sessionStore.sessions[sessionID] = deletedError;
+    }
+
+    response.render("login.hbs", {
+        title: "Login",
+        heading: "Log In",
+        failureFlag: failureFlag,
+        failureMessage: failureMessage
     });
 });
 

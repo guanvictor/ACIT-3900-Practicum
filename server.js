e2e5daec94bc3dbc75e17feab918968d16955cda
Michael-Vataga <michael.vataga@gmail.com>
2019-10-10 20:46:16 -0700

    Created admin panel. Can navigate to it through localhost:8080/admin

--- server.js
+++ server.js
@@ -82,3 +82,11 @@ app.get('/contact', (request, response) => {
     });
 });
 
+//Admin Page
+app.get('/admin', (request, response) => {
+    response.render("admin.hbs", {
+        title: "Admin Panel",
+        heading: "Admin Panel"
+    });
+});
+

65a6616ee5fcfa411e00cb7e3f4899dad35988d9
Cathy Chan <chan.cathy@hotmail.ca>
2019-11-01 00:52:35 -0700

    admin about event page fully

--- admin.js
+++ admin.js
@@ -2,8 +2,12 @@ const formidable = require('formidable');
 const express = require("express");
 const fs = require("fs");
 
+const db = require("./database.js");
+
 const router = express.Router();
  
+// adds file
+// used in admin/webcontent/home
 const upload = (request, response) => {
     let form = new formidable.IncomingForm();
     let path = '';
@@ -27,6 +31,8 @@ const upload = (request, response) => {
     response.redirect('/admin/webcontent/home');
 };
 
+// deletes file
+// used in admin/webcontent/home
 const deleteFile = async (request, response) => {
     let file_path = await request.body.path;
     
@@ -40,7 +46,52 @@ const deleteFile = async (request, response) => {
     response.redirect('/admin/webcontent/home');
 };
 
+// updates about event page
+// used in admin/webcontent/about
+const updateAbout = async (request, response) => {
+    let con = db.getDb();
+    let sql = '';
+
+    let title = request.body.aboutTitle;
+    let date = request.body.aboutDate;
+    let timeStart = request.body.about_hr1;
+    let timeEnd = request.body.about_hr2;
+    let desc = request.body.about_desc;
+
+    //TODO: add if (gmaps is empty), check syntax if not
+    let gmaps = request.body.aboutGmaps;
+
+
+    if (gmaps != '' && gmaps != undefined) {
+        //TDOO: check regex
+        let regex = /https:[^"]*/;
+        let array = regex.exec(gmaps);
+
+        if (array != null){
+            let link = array[0];
+            sql = "UPDATE about_event SET googleMaps=?";
+            con.query(sql, link, (err, result) => {
+                if (err) throw (err);
+
+                console.log("Successfully changed Google Maps iframe");
+            });
+        }
+    }
+
+    
+    sql = "UPDATE about_event SET title=?, date=?, startTime=?, endTime=?, description=?";
+    let values = [title, date, timeStart, timeEnd, desc];
+    con.query(sql, values, (err, result) => {
+        if (err) throw(err);
+
+        console.log("Successfully updated About Event page details");
+    });
+
+    response.redirect('/admin/webcontent/about');
+};
+
 router.post("/upload", upload);
 router.post("/deleteFile", deleteFile);
+router.post("/updateAbout", updateAbout);
 
 module.exports = router;

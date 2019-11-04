dcc5695ff8e3cd9234c875d0b48316843298dc4a
guanvictor <guanvictor@hotmail.com>
2019-11-03 17:07:22 -0800

    Start ignoring api_key

--- api_key.js
+++
@@ -1,9 +0,0 @@
-module.exports.auth = {
-    auth: {
-        //api_key: 'c6ea6648aa1a2894c4bc326e959c093a-816b23ef-03d253aa',
-        //domain: 'sandbox41fa3dd81c83419e8d835b1f9ce51d6d.mailgun.org'
-        api_key: '4e6456aa56f408e85b48e2ce6db89dbd-816b23ef-08188715',
-        domain: 'sandboxf42b71ccb0414ed687383cabc8554b09.mailgun.org'
-    }
-}
-

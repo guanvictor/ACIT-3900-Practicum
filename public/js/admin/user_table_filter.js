72fcbf29b090516325e0d4d434e384a5bef7da51
orangeappless <wilf.ta0072@gmail.com>
2019-11-14 21:57:03 -0800

    fixed user filter Co-authored-by: cat-box <chan.cathy@hotmail.ca>

--- public/js/admin/user_table_filter.js
+++ public/js/admin/user_table_filter.js
@@ -3,11 +3,11 @@ filterUsers = () => {
     let name = $(`input[name=${filter}]:checked`).prop('id') || '';
 
     $('.account_table').hide();
-    $(`.account_table:contains(${name})`).show();
-    
+    $(`.account_table .${filter}:contains(${name})`).closest("td").parent().show();
+
     // Show all plantClassifications
     let radioValue = $(`input[type=radio][name=${filter}]:checked`).attr('id');
-    
+
     if (radioValue == "all") {
         $('.account_table').show();
     }
